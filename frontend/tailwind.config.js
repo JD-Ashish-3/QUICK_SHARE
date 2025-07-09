b2fee73ed673e62b9c002f1145fa72c6910296e5
JD-Ashish-3 <ashishba33@gmail.com>
2025-07-09 20:23:50 +0530

    lastt

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,16 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: [
-  "./index.html",
-  "./src/**/*.{js,ts,jsx,tsx}",
-  "./components/**/*.{js,ts,jsx,tsx}",
-  "./pages/**/*.{js,ts,jsx,tsx}",
-  "./lib/**/*.{js,ts,jsx,tsx}",
-],
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./lib/**/*.{js,ts,jsx,tsx}",
+  ],
+  safelist: [
+    'bg-purple-500',
+    'text-purple-500',
+    'hover:bg-purple-600',
+  ],
   theme: {
     extend: {},
   },
